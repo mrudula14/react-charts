81989bb88845a726733aaf8f739a9fc5dc34c483
Mrudula <varademrudula@gmail.com>
2024-05-28 22:57:38 +0530

    router config new

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -31,7 +31,7 @@ function Sidebar() {
                           </li>
                           <li>
                               <NavLink
-                              to="/react-charts//about"
+                              to="/react-charts/about"
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? 'text-orange-700': 'text-gray-700'} lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                   }
